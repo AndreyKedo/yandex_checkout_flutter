6789f623927b34d3d0bef6dd33a80c20fc2b9b8a
andrexkedo@gmail.com <KoRvO_43-aNdReY>
2020-07-19 17:00:22 +0700

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,6 +13,33 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String _data = '';
 
+  void _pay() async {
+    final PaymentParameters param = PaymentParameters(
+        amount: Amount(currency: Currency.RUB, value: 20.99),
+        title: 'Anything',
+        subTitle: 'Subtitle anything',
+        clientApplicationKey: 'test_token',
+        paymentMethodTypes: {
+          PaymentMethodType.SBERBANK,
+          PaymentMethodType.BANK_CARD,
+          PaymentMethodType.GOOGLE_PAY,
+        },
+        googlePayParameters: {
+          GooglePayParameters.MASTERCARD,
+          GooglePayParameters.VISA
+        },
+        shopId: '000000');
+    final Result result = await YandexCheckout().startTestCheckout(param);
+    if(result.hasError)
+      setState(() => _data = 'Error');
+    else if(result.hasData){
+      final data = result.data;
+      setState(() => _data = data.token);
+    }
+    else
+      setState(() => _data = 'Checkout cancel');
+  }
+
   @override
   Widget build(BuildContext context) => MaterialApp(
     home: Scaffold(
@@ -27,32 +54,7 @@ class _MyAppState extends State<MyApp> {
             ),
             OutlineButton(
                 child: const Text('Buy anything...'),
-                onPressed: () async {
-                  final PaymentParameters param = PaymentParameters(
-                      amount: Amount(currency: Currency.RUB, value: 20.99),
-                      title: 'Anything',
-                      subTitle: 'Subtitle anything',
-                      clientApplicationKey: 'test_token',
-                      paymentMethodTypes: {
-                        PaymentMethodType.SBERBANK,
-                        PaymentMethodType.BANK_CARD,
-                        PaymentMethodType.GOOGLE_PAY,
-                      },
-                      googlePayParameters: {
-                        GooglePayParameters.MASTERCARD,
-                        GooglePayParameters.VISA
-                      },
-                      shopId: '000000');
-                  final Result result = await YandexCheckout().startTestCheckout(param);
-                  if(result.hasError)
-                    setState(() => _data = 'Error');
-                  else if(result.hasData){
-                    final data = result.data;
-                    setState(() => _data = data.token);
-                  }
-                  else
-                    setState(() => _data = 'Checkout cancel');
-                }
+                onPressed: ()  => _pay()
             )
           ],
         ),
